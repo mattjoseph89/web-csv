45c4f3021d2f04afc49e01cf9f8dea12176cd3ed
Sarah Cawthon <sarahecawthon@gmail.com>
2017-08-08 14:33:54 -0500

    newer

--- main.rb
+++ main.rb
@@ -1,10 +1,22 @@
 require "csv"
 require "sinatra"
+require "pry"
 require_relative "./csv.rb"
 
 get '/' do
   accounts = process_csv
-  erb(:main)
+  erb(:main, :locals => {:accounts => accounts})
 end
 
+get '/full' do
+  erb(:all, :locals => {:accounts => accounts})
+end
 
+get '/priya' do
+  erb(:accounts)
+end
+
+# get '/accounts' do
+#   @name = params['name']
+#   erb(:accounts)
+# end
